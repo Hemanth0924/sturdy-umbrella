c6a87a41dcc8e0266769eef85b35cd9b60a4a544
Hemanth0924 <88907972+Hemanth0924@users.noreply.github.com>
2022-12-17 18:41:04 -0800

    Add files via upload

---
+++ screens/Transaction.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import * as Permissions from "expo-permissions";
+import {BarCodeScanner} from "expo-barcode-scanner"
+
+export default class TransactionScreen extends Component {
+constructor (props){
+  super (props);
+  this.state = {
+    domState :"normal",
+    hasCameraPermissions : null,
+    scanned : false,
+    scannedData : "" 
+  };
+}
+ 
+getCameraPermissions = async domState =>{
+  const{status} = await Permissions.askAsync(Permissions.CAMERA);
+  
+this.setState({
+  hasCameraPermissions: status === "granted",
+domState: domState,
+scanned:false
+});
+  };
+
+handleBarCodeScanned = async({type, data}) =>{
+  this.setState({
+  scannedData : data, 
+  domState: "normal",
+  scanned: true
+  });
+};
+
+  render() {
+    const{domState, hasCameraPermissions, scannedData, scanned} = this.state;
+    if(domState === "scanner"){
+      return(
+        <BarCodeScanner
+        onBarCodeScanned = { scanned ? undefined : this.handleBarCodeScanned}
+        style = {StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+        {hasCameraPermissions ? scannedData : "request for camera permissions"}
+          
+        </Text>
+<TouchableOpacity
+style = {[styles.button, {marginTop:25}]}
+onPress = {() => this.getCameraPermissions("scanner")}
+>
+ <Text style = {styles.buttonText}> Scan QR Code HERE
+   </Text> 
+  
+</TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5653D4"
+  },
+  text: {
+    color: "#ffff",
+    fontSize: 30
+  },
+  button : {
+    width: 43,
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F48D20",
+    borderRadius: 15
+},
+buttonText: {
+  fontSize:24,
+  color: "#000000"
+
+}
+});
